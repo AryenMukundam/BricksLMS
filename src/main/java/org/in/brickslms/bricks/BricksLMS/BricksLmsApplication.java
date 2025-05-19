204c826cd6c8fd5699e350ed36fca315c48dbd65
AryenMukundam <aryenmukundam@gmail.com>
2025-05-19 17:16:14 +0530

    Initial commit

---
+++ src/main/java/org/in/brickslms/bricks/BricksLMS/BricksLmsApplication.java
@@ -0,0 +1,13 @@
+package org.in.brickslms.bricks.BricksLMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricksLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricksLmsApplication.class, args);
+	}
+
+}
